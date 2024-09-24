e414005d77368aa0c41e15f15bc4148a4f7f7237
zabir emu <zabirraihan570@gmail.com>
2024-09-24 23:35:49 +0600

    update login file

--- resources/views/admin/auth/layouts/login.blade.php
+++ resources/views/admin/auth/layouts/login.blade.php
@@ -30,10 +30,7 @@
         @include('admin.components.button', ['label' => 'Github'])
         @include('admin.components.button', ['label' => 'Twitter'])
 
-        @include('admin.components.forgetPassword', [
-            'label' => 'Forget Password?',
-            'url' => 'password.request',
-        ])
+
         @include('admin.components.forgetPassword', [
             'label' => 'Create Account?',
             'url' => 'register',
